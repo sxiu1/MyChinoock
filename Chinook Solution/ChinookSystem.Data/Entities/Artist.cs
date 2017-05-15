52f6f02290368f711fb79a816a6eda59dfb7fff5
Allan Anderson <allana@shaw.ca>
2017-05-14 23:43:00 -0700

    Project setup day2

---
+++ Chinook Solution/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region  Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Artists")]
+    public class Artist
+    class Artist
+    {
+        [Key]
+        public int Artistid { get; set; }
+        public string Name { get; set; }
+        //Navigatin property used by LINQ
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
---
+++ Chinook Solution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    //This is an interal class for security reasons
+    //Access is restricted to within this class library project
+    //Inherits DbContext for Entity Framwork,whch requires
+    //System.Data.Entity
+    internal class ChinookCont :DbContext 
+    {
+        //Pass the connnecting string name to the 
+        // DbContext usng : base()
+        public ChinookContext() : base("ChinookDB")
+   { }
+    }
+}
