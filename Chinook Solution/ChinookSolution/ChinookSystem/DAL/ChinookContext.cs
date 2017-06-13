bb094a33e9c7303c22bb28697e9e52ee6b715bca
Allan Anderson <allana@shaw.ca>
2017-06-12 20:10:25 -0700

    9

--- Chinook Solution/ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
+++ Chinook Solution/ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 #region Additional Namespaces
-using System.Data.Entity;
 using ChinookSystem.Data.Entities;
 #endregion
 
@@ -24,7 +18,30 @@ namespace ChinookSystem.DAL
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Album> Albums { get; set; }
         public DbSet<Track> Tracks { get; set; }
-        public DbSet<Employee> Employees{ get; set; }
+        public DbSet<Employee> Employees { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<MediaType> MediaTypes { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<PlaylistTrack> PlaylistTracks { get; set; }
+    }
+
+    internal class DbContext
+    {
+        private string v;
+
+        public DbContext(string v)
+        {
+            this.v = v;
+        }
+    }
+
+    public class Employee
+    {
+    }
+
+    public class DbSet<T>
+    {
     }
 }
+
