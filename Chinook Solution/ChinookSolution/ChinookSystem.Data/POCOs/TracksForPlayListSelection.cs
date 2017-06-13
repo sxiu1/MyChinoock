536153e93e7cb1929953817eebd5ec3576bf7437
Allan Anderson <allana@shaw.ca>
2017-06-12 20:01:10 -0700

    9

--- Chinook Solution/ChinookSolution/ChinookSystem.Data/POCOs/TracksForPlayListSelection.cs
+++ Chinook Solution/ChinookSolution/ChinookSystem.Data/POCOs/TracksForPlayListSelection.cs
@@ -4,23 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-#region Additional Namespaces
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-#endregion
-
-
 namespace ChinookSystem.Data.POCOs
 {
-  public   class TracksForPlayListSelection
+    public class TracksForPlaylistSelection
     {
         public int TrackId { get; set; }
         public string Name { get; set; }
         public string MediaName { get; set; }
-        public string GenerName { get; set; }
-        public string composer { get; set; }
-        public string Millisecongs { get; set; }
-        public string Bytes { get; set; }
-        public string  UnitPrice   { get; set; }
+        public string GenreName { get; set; }
+        public string Composer { get; set; }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+
     }
 }
---
+++ Chinook Solution/ChinookSolution/ChinookSystem.Data/POCOs/UserPlaylistTrack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Data.POCOs
+{
+    public class UserPlaylistTrack
+    {
+        public int TrackId { get; set; }
+        public string Title { get; set; }
+        public int TimeLength { get; set; }
+        public int? Size { get; set; }
+        public decimal UnitPrice
+        {
+            get; set;
+        }
+    }
+}
